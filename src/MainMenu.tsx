1d7323d1cfaf16a29b686649aaa1c7c7a6b80bb0
Roman Zaichenko <romanzaichenko33@gmail.com>
2025-03-25 23:06:30 +0200

    Main menu created.

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -1,16 +1,29 @@
 import './styles/MainMenu.css'
 import MenuButton from "./MenuButton.tsx";
+import * as React from "react";
 
 
 function MainMenu() {
+    const [hidden, setHidden] = React.useState(false);
+
     return (
         <main className='menu'>
             <h1 className="menu-title">Shiogi</h1>
-            <MenuButton buttonName={"Player"}/>
-            <MenuButton buttonName={"Settings"}/>
-            <MenuButton buttonName={"Rules"}/>
-            <MenuButton buttonName={"Profile"}/>
-            <MenuButton buttonName={"Exit"}/>
+            <MenuButton buttonName={"Play"} clickHandler={() => {
+                setHidden(true);
+            }}/>
+            <MenuButton buttonName={"Profile"} clickHandler={() => {
+                setHidden(true);
+            }}/>
+            <MenuButton buttonName={"Rules"} clickHandler={() => {
+                setHidden(true);
+            }}/>
+            <MenuButton buttonName={"Settings"} clickHandler={() => {
+                setHidden(true);
+            }}/>
+            <MenuButton buttonName={"Exit"} clickHandler={() => {
+                setHidden(true);
+            }}/>
         </main>
 
     );
--- src/MenuButton.tsx
+++ src/MenuButton.tsx
@@ -1,11 +1,15 @@
+import './styles/MenuButton.css'
+import * as React from "react";
 
 interface MenuButtonProps {
     buttonName: string;
+    clickHandler: () => void;
 }
 
-function MenuButton({ buttonName }: MenuButtonProps) {
+function MenuButton({ buttonName, clickHandler }: MenuButtonProps) {
     return (
-        <button className="menu-button">{buttonName}</button>
+        <button style={{"--button-content": `"${buttonName}"`} as React.CSSProperties} className="menu-button"
+        onClick={() => clickHandler()}></button>
     );
 }
 
